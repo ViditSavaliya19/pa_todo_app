89b0ee5c7aaeb295efff24872411fb57690a1f31
r29 <rnwprogrammingvidit@gmail.com>
2023-08-07 19:58:05 +0530

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pa_todo_app/screen/addnote_screen.dart';
 import 'package:pa_todo_app/screen/home_screen.dart';
+import 'package:pa_todo_app/screen/notes_screen.dart';
 
 void main() {
   runApp(
@@ -9,7 +10,8 @@ void main() {
       theme: ThemeData(useMaterial3: true),
        debugShowCheckedModeBanner: false,
       routes: {
-        '/':(context) => HomeScreen(),
+        // '/':(context) => HomeScreen(),
+        '/':(context) => NotesScreen(),
         'note':(context) => AddNotesScreen(),
       }
     ),
--- lib/screen/addnote_screen.dart
+++ lib/screen/addnote_screen.dart
@@ -14,6 +14,71 @@ class _AddNotesScreenState extends State<AddNotesScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text("Add Notes"),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Title",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 50,
+                alignment: Alignment.center,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColorLight,
+                    borderRadius: BorderRadius.circular(10)),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    hintStyle: TextStyle(color: Colors.black26),
+                    hintText: "Speak Title...",
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Notes",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColorLight,
+                    borderRadius: BorderRadius.circular(10)),
+                child: const TextField(
+                  maxLines: 10,
+                  decoration: InputDecoration(
+                    hintStyle: TextStyle(color: Colors.black26),
+                    hintText: "Speak Notes...",
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
         ),
       ),
     );
